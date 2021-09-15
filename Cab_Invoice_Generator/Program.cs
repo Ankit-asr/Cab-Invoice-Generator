47c1aebfe0e7739750399fa322d4a6fbf83d55b6
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-16 02:49:16 +0530

    [Ankit] Add: Given Distance and Time the Invoice Generator returns total Fare for journey

--- Cab_Invoice_Generator/Program.cs
+++ Cab_Invoice_Generator/Program.cs
@@ -6,7 +6,11 @@ namespace Cab_Invoice_Generator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Cab Invoice Generator") ;
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL_RIDE);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
+
         }
     }
 }
